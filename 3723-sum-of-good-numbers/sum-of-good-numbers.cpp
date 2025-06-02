48ace8ad092968bcb7ab29775a5fd190bc62633c
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-02 16:06:42 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (24.04%) - LeetSync

--- 3723-sum-of-good-numbers/sum-of-good-numbers.cpp
+++ 3723-sum-of-good-numbers/sum-of-good-numbers.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int sumOfGoodNumbers(vector<int>& nums, int k) {
-        int n = nums.size();
-        int sum = 0;
-       
-        for (int i = 0; i < n; i++) {
-            bool lgood = true;
-            bool rgood = true;
-            if (i - k >= 0)
-             {
-                if (!(nums[i] > nums[i - k]))
-                    lgood = false;
-            }
-            if (i + k < n) 
-            {
-                if (!(nums[i] > nums[i + k]))
-                    rgood = false;
-            }
-            if (lgood && rgood) 
-            {
-                sum += nums[i];
-            }
+        int n=nums.size();
+       int sum=0;
+       int count=0;
+       for(int i=0;i<n;i++)
+       {
+        bool lgood=true;
+        bool rgood=true;
+        if(i-k>=0)
+        {
+            if(!(nums[i]>nums[i-k]))
+            lgood=false;
         }
-    return sum;
-}
+        if(i+k<n)
+        {
+            if(!(nums[i]>nums[i+k]))
+            rgood=false;
+        }
+        if(lgood && rgood)
+        {
+            sum+=nums[i];
+        }
+       } 
+       return sum;
+    }
 };
