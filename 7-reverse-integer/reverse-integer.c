74a55c2e1f2f69f256cac97de5bb99d9551315dc
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-02 11:37:03 +0530

    Time: 6 ms (23.52%) | Memory: 8.4 MB (98.45%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.c
@@ -0,0 +1,12 @@
+int reverse(int x){
+  long long reverse=0;
+  while(x!=0)
+  {
+    int digit=x%10;
+    x/=10;
+    reverse= reverse*10+digit;
+    if(reverse>INT_MAX || reverse<INT_MIN)
+    return 0;
+  }
+  return (int)reverse;
+}
