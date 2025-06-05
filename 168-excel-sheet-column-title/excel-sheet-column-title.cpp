eb8a148f51caf0f47d8a48b5fda492a5167f5815
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-05 16:21:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (90.62%) - LeetSync

---
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string res="";
+        while(columnNumber>0)
+        {
+            columnNumber--;
+            int remainder=columnNumber%26;
+            char ch='A'+remainder;
+            res=ch+res;
+           columnNumber/=26;
+        }
+        return res;
+    }
+};
