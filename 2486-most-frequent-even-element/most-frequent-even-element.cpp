36afc90e827076d4f15cc5b1943d0acdb966a7cb
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-08 18:18:10 +0530

    Time: 36 ms (28.52%) | Memory: 133.2 MB (5.08%) - LeetSync

--- 2486-most-frequent-even-element/most-frequent-even-element.cpp
+++ 2486-most-frequent-even-element/most-frequent-even-element.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int mostFrequentEven(vector<int>& nums) {
+        int result=-1;
         vector<int>freq(100001,0);
         int maxfreq=0;
-        int result=-1;
         for(auto& n:nums)
         {
             if(n%2==0)
             {
                 freq[n]++;
-            if(freq[n]>maxfreq || freq[n]==maxfreq && n<result)
-            {
-                maxfreq=freq[n];
-                result=n;
+                if(freq[n]>maxfreq || freq[n]==maxfreq && n<result)
+                {
+                    result=n;
+                    maxfreq=freq[n];
+                }
             }
-            }
-            
         }
         return result;
     }
