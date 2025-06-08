a349f28eab2925db4357ed63ebe1b8db707dd69b
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-08 15:54:07 +0530

    Time: 5 ms (39.25%) | Memory: 11.8 MB (52.03%) - LeetSync

--- 383-ransom-note/ransom-note.cpp
+++ 383-ransom-note/ransom-note.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-       vector<int>ans(26,0);
-       for(char ch:magazine)
-       {
-        ans[ch-'a']++;
-       }
-       for(char ch:ransomNote)
-       {
-        if(ans[ch-'a']==0)return false;
-        ans[ch-'a']--;
-       }
-       return true;
+        unordered_map<char,int>freq;
+        for(char ch:magazine)
+        {
+            freq[ch]++;
+        }
+        for(char ch:ransomNote)
+        {
+            if(freq[ch]==0)return false;
+            freq[ch]--;
+        }
+        return true;
     }
 };
