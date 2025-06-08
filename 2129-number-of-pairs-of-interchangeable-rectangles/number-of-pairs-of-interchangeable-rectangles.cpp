f9f76e7dfbaa8311a7e58e40f711a4fcc147d61d
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-08 18:03:48 +0530

    Time: 194 ms (16.13%) | Memory: 159.6 MB (22.49%) - LeetSync

--- 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
+++ 2129-number-of-pairs-of-interchangeable-rectangles/number-of-pairs-of-interchangeable-rectangles.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     long long interchangeableRectangles(vector<vector<int>>& rectangles) {
-        unordered_map<string, long long> ratioCount;
-        long long result = 0;
-
-        for (auto& rect : rectangles) {
-            int w = rect[0], h = rect[1];
-            int g = __gcd(w, h);
-            w /= g;
-            h /= g;
-            string key = to_string(w) + "/" + to_string(h); 
-            result += ratioCount[key];
-            ratioCount[key]++;
+        long long result=0;
+        unordered_map<string,int>rcount;
+        for(auto&rect:rectangles)
+        {
+            int w=rect[0];
+            int h=rect[1];
+            int g=__gcd(w,h);
+            w=w/g;
+            h=h/g;
+            string key=to_string(w)+"/"+to_string(h);
+            result+=rcount[key];
+            rcount[key]++;
         }
-
         return result;
     }
 };
