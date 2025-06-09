38f24255031e01be96cb54c6c8e0f4635ff23cf9
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-09 11:30:39 +0530

    Time: 10 ms (5.26%) | Memory: 14.1 MB (68.00%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -7,7 +7,7 @@ public:
             for(int j=0;j<nums2.size();j++)
             {
                 if(nums1[i]==nums2[j])
-                s.insert(nums1[i]);
+                s.insert(nums2[j]);
             }
         }
         vector<int>res;
