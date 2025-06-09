827cd3e2fd0e725b7bf5a315882d682b26c35d77
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-09 12:31:14 +0530

    Time: 2 ms (51.40%) | Memory: 9.6 MB (9.71%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -3,15 +3,17 @@ public:
     bool isIsomorphic(string s, string t) {
         unordered_map<char,int>ss;
         unordered_map<char,int>tt;
-        for(int i=0;i<s.size();i++)
-        {
-            if(ss.find(s[i])==ss.end())
-            ss[s[i]]=i;
-            if(tt.find(t[i])==tt.end())
-            tt[t[i]]=i;
-            if(ss[s[i]]!=tt[t[i]])
-            return false;
-        }
-return true;
-    }
+       for(int i=0;i<s.size();i++)
+       {
+        char sc=s[i];
+        char tc=t[i];
+        if(ss.count(s[i]) && ss[s[i]]!=tc)
+        return false;
+         if(tt.count(t[i]) && tt[t[i]]!=sc)
+        return false;
+        ss[s[i]]=tc;
+        tt[t[i]]=sc;
+       }
+       return true;
+       }
 };
