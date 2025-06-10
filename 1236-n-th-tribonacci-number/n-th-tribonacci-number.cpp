6d82603fa9549a671cc5eec6653f041948d7d9c5
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-10 10:20:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (84.99%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -3,13 +3,12 @@ public:
     int tribonacci(int n) {
         if(n<=1)return n;
         if(n==2)return 1;
-            int a=0;
-            int b=1;
-            int c=1;
-          
+        int a=0;
+        int b=1;
+        int c=1;
         for(int i=3;i<=n;i++)
         {
-          int  res=a+b+c;
+            int res=a+b+c;
             a=b;
             b=c;
             c=res;
