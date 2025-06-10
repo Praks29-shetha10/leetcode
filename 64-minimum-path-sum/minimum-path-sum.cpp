d98e5267ea82a36a31f24134226d97b845506015
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-10 10:56:31 +0530

    Time: 7 ms (6.07%) | Memory: 16.4 MB (13.57%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -7,7 +7,7 @@ public:
         dp[0][0]=grid[0][0];
         for(int i=1;i<n;i++)
         {
-            dp[i][0]=dp[i-1][0]+grid[i][0];
+            dp[i][0]=dp[i-1][0]+grid[i][0];            
         }
         for(int j=1;j<m;j++)
         {
@@ -20,6 +20,6 @@ public:
                 dp[i][j]=grid[i][j]+min(dp[i-1][j],dp[i][j-1]);
             }
         }
-      return dp[n-1][m-1];
+        return dp[n-1][m-1];
     }
 };
