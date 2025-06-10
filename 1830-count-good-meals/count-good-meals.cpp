7e70705e224255138fe5c20c54ce9f0aa627bfee
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-10 12:10:17 +0530

    Time: 209 ms (60.80%) | Memory: 76.3 MB (57.60%) - LeetSync

--- 1830-count-good-meals/count-good-meals.cpp
+++ 1830-count-good-meals/count-good-meals.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int countPairs(vector<int>& deliciousness) {
-        const int MOD = 1e9 + 7;
+    int countPairs(vector<int>& d) {
         unordered_map<int,int>freq;
-        int c=0;
-        for(int val:deliciousness)
+        const int MOD=1e9+7;
+        int count=0;
+        for(int val:d)
         {
-            for(int power=1;power<=2097152;power*=2)
+            for(int i=1;i<=2097152;i*=2)
             {
-                int pair=power-val;
-                if(freq.count(pair))
+                int ans=i-val;
+                if(freq.count(ans))
                 {
-                    c=(c+freq[pair])%MOD;
+                    count=(count+freq[ans])%MOD;
                 }
             }
             freq[val]++;
         }
-        return c;
+        return count;
     }
 };
