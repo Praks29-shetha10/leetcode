8894ce19eb285840d2bc3f0fff152a0cbfa80e49
Praks29-shetha10 <praksshetha50@gmail.com>
2025-06-10 09:42:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (53.83%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -5,16 +5,15 @@ public:
         int m=obstacleGrid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,0));
 
-        if(obstacleGrid[0][0] == 1) return 0;
-        dp[0][0]=1; 
+        if(obstacleGrid[0][0]==1)return 0;
+        dp[0][0]=1;
 
         for(int i=1;i<n;i++)
         {
-            if(obstacleGrid[i][0]==0 && dp[i-1][0]==1)
+            if(obstacleGrid[i][0] == 0 && dp[i-1][0] == 1)
             dp[i][0]=1;
             else
             dp[i][0]=0;
-
         }
 
         for(int j=1;j<m;j++)
